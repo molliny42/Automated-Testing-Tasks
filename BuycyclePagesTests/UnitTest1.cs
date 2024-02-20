7f620ab2c432a7fead751b25ca84d9555d6cb226
molliny42 <molliny42@gmail.com>
2024-02-21 00:21:09 +0100

    Added initial files for BuycyclePagesTests project

---
+++ BuycyclePagesTests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+
+namespace BuycyclePagesTests;
+
+public class Tests
+{
+    private IWebDriver _webDriver;
+
+    [SetUp]
+    public void Setup()
+    {
+        _webDriver = new OpenQA.Selenium.Chrome.ChromeDriver();
+
+        _webDriver.Manage().Window.Maximize();
+        _webDriver.Navigate().GoToUrl("https://buycycle.com");
+       
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    //
+    [TearDown]
+    public void TearDown()
+    {
+        _webDriver.Quit();
+        _webDriver.Dispose();
+    }
+}
