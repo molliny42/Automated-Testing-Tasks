71c8f215d9430f42cfbb672da32928a171b5aeec
molliny42 <molliny42@gmail.com>
2024-02-21 18:01:59 +0100

    feat: Add UserNameForTests class with login and password

---
+++ BuycyclePagesTests/UserNameForTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BuycyclePagesTests
+{
+	public class UserNameForTests
+	{
+        public static string StartLogin { get; set; } = "molliny42@gmail.com";
+        public static string StartLoginPassword { get; set; } = "hpmor107";
+    }
+}
+
